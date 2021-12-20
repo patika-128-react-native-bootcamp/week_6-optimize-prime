45241f4d98c861cc7c02404d909b262abfb6178f
osmanalpercam <63802259+OsmanAlperCAM@users.noreply.github.com>
2021-12-20 21:37:21 +0300

    Theme detection is done when the app is opened

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Provider from '../context/Provider';
 
 import Home from '../Pages/Home';
 import Detail from '../Pages/Detail';
@@ -9,13 +10,14 @@ const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Settings" component={Detail} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Settings" component={Detail} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
-}
+};
 export default Navigation;
-
--- src/context/reducers.js
+++ src/context/reducers.js
@@ -1,7 +1,6 @@
 export default (state, action) => {
   switch (action.type) {
     case 'CHANGE_APP_THEME':
-      console.log(state.theme);
       return {...state, theme: action.payload.theme};
 
     default:
