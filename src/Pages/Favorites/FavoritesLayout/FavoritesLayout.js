0a3dd0623810e5a2b0fb1ec07f9c1fd75e04032c
omur aydin <ommuraydin@gmail.com>
2021-12-23 15:28:10 +0300

    Corrections on favorites

--- src/Pages/Favorites/FavoritesLayout/FavoritesLayout.js
+++ src/Pages/Favorites/FavoritesLayout/FavoritesLayout.js
@@ -1,38 +1,17 @@
 import React from 'react';
-import { View, Text, FlatList, TouchableWithoutFeedback, Image } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
-import styles from './FavoritesLayout.style';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { View, FlatList } from 'react-native';
+
+import ThumbnailCard from '../../../components/ThumbnailCard';
 
 const FavoritesLayout = ({ onItemPress, onPress, favoritesList }) => {
-  const theme = useSelector(state => state.theme);
-  const thumbnailSize = '/portrait_fantastic.jpg';
-
-
-
-
   const renderComics = ({ item }) => {
     return (
-      <TouchableWithoutFeedback onPress={() => onItemPress(item)}>
-        <View style={styles[theme].container}>
-          <View style={styles[theme].inner_container}>
-            <View style={styles[theme].favContainer}>
-              <TouchableWithoutFeedback onPress={onPress}>
-                <Icon name="star" size={37} color="white" />
-              </TouchableWithoutFeedback>
-            </View>
-            <Image
-              style={styles[theme].thumbnail}
-              source={{ uri: `${item.thumbnail.path}${thumbnailSize}` }}
-            />
-            <View style={styles[theme].title_container}>
-              <Text numberOfLines={2} style={styles[theme].title}>
-                {item.title}
-              </Text>
-            </View>
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
+      <ThumbnailCard
+        thumbnail={item.thumbnail.path}
+        title={item.title}
+        onThumbnailCardPress={() => onItemPress(item)}
+        onPress={onPress}
+      />
     );
   };
 
--- src/Pages/Favorites/FavoritesLayout/FavoritesLayout.style.js
+++ src/Pages/Favorites/FavoritesLayout/FavoritesLayout.style.js
@@ -4,48 +4,5 @@ import spacing from '../../../styles/spacing';
 const { width, height } = Dimensions.get('window');
 
 const baseStyles = StyleSheet.create({
-  container: {
-    padding: spacing.tiny,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: width * 0.5,
-  },
-  inner_container: {
-    width: 168,
-    margin: spacing.tiny,
-    borderWidth: 1,
-    alignItems: 'center',
-  },
-  favContainer: {
-    position: "absolute",
-    zIndex: 10,
-    right: 10,
-    top: 10,
-  },
-  thumbnail: {
-    width: 168,
-    height: 252,
-    resizeMode: 'stretch'
-  },
-  title_container: {
-    justifyContent: 'center',
-    height: 50,
-    padding: spacing.tiny,
-  },
-  title: {
-    fontSize: 15,
-    flexWrap: 'wrap',
-    fontFamily: 'Proxima Nova Semibold',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
 
-export default {
-  light: StyleSheet.create({
-    ...baseStyles,
-  }),
-  dark: StyleSheet.create({
-    ...baseStyles,
-  }),
-};
+});
