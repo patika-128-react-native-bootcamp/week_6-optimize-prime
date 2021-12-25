6a9789c2faf191ba8dc9460a44c9a7adb67ce071
omur aydin <ommuraydin@gmail.com>
2021-12-25 20:22:37 +0300

    language pack adjustment

--- src/languages/i18n.js
+++ src/languages/i18n.js
@@ -5,6 +5,7 @@ import spanish from "./spanish.json"
 import { initReactI18next } from "react-i18next";
 
 i18next.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
   lng: "en",
   fallbackLng: "en",
   resources: {
