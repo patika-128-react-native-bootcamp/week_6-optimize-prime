420016813964e495b6719b861d21d8e8bc7b30ce
omur aydin <ommuraydin@gmail.com>
2021-12-25 16:22:27 +0300

    characterDetail page styling done

--- src/Pages/ChracterDetail/ChracterDetailLayout/ChracterDetailLayout.style.js
+++ src/Pages/ChracterDetail/ChracterDetailLayout/ChracterDetailLayout.style.js
@@ -1,11 +1,50 @@
-import {StyleSheet, Dimensions} from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+import spacing from '../../../styles/spacing';
+import fontSize from '../../../styles/fontSize';
 
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 const baseStyles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  
+  inner_container: {
+    alignItems: 'center',
+    resizeMode: 'contain',
+    position: 'relative'
+  },
+  thumbnail: {
+    width: width,
+    height: width * 1.3,
+
+  },
+  title: {
+    alignSelf: 'flex-start',
+    fontWeight: 'bold',
+    fontFamily: 'Proxima Nova Semibold',
+    fontSize: fontSize.large,
+    textAlign: 'left',
+    color: "white",
+    position: "absolute",
+    bottom: 0,
+    padding: spacing.large,
+    fontSize: fontSize.huge,
+
+  },
+  description: {
+    alignSelf: 'flex-start',
+    fontFamily: 'Proxima Nova Semibold',
+    fontSize: fontSize.normal,
+    textAlign: "justify",
+    color: 'white',
+    padding: spacing.large,
+  },
+  comicsTitle: {
+    color: "white",
+    fontSize: fontSize.huge,
+    fontFamily: 'Proxima Nova Semibold',
+
+  }
+
 });
 
 export default {
