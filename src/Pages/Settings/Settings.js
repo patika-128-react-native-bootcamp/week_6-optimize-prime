87296c484bdf2edf227b4c81ee012bc342dd4374
omur aydin <ommuraydin@gmail.com>
2021-12-25 20:26:19 +0300

    language option added  to settings

--- src/Pages/Settings/Settings.js
+++ src/Pages/Settings/Settings.js
@@ -3,22 +3,28 @@ import { View, Text } from 'react-native';
 import SwitchSelector from "react-native-switch-selector";
 import { useTranslation } from "react-i18next";
 
-const options = [
+const langOptions = [
   { label: "English", value: "en" },
   { label: "Deutsch", value: "de" },
   { label: "Spanish", value: "es" }
+]
+const themeOptions = [
+  { label: "Light", value: "light" },
+  { label: "Dark", value: "dark" },
 ]
 
 const Settings = () => {
   const { t, i18n } = useTranslation();
   return (
-    <View style={{justifyContent: 'center', alignItems: 'center', height: '100%'}}>
-      <SwitchSelector options={options} initial={0} hasPadding
+    <View style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+      <SwitchSelector options={langOptions} initial={0} hasPadding bold buttonColor={"red"} 
+
         onPress={(languages) => {
           i18n.changeLanguage(languages);
         }}
       />
       <Text>{t("SettingsText")}</Text>
+      <SwitchSelector options={themeOptions} initial={0} hasPadding bold buttonColor={"red"}/>
     </View>
   );
 }
