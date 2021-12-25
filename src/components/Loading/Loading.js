28310efdf513ac10600bac8e4a427554dbced43d
omur aydin <ommuraydin@gmail.com>
2021-12-25 21:24:26 +0300

    loading gif rescaled

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import LottieView from 'lottie-react-native';
-
+import styles from './Loading.style';
 const Loading = props => {
   return (
-    <LottieView source={require('../../Assets/loading.json')} autoPlay loop />
+    <View style={styles.loadingContainer}>
+      <LottieView source={require('../../Assets/loading.json')} autoPlay loop style={styles.loading} />
+    </View>
   );
 };
 export default Loading;
