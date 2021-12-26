4a25e73be58177d6bef4d6ddb9f3562b09f9c86b
osmanalpercam <63802259+OsmanAlperCAM@users.noreply.github.com>
2021-12-26 19:38:22 +0300

    Theme settings have been saved

--- src/Pages/Settings/Settings.js
+++ src/Pages/Settings/Settings.js
@@ -1,41 +1,76 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import SwitchSelector from "react-native-switch-selector";
-import { useTranslation } from "react-i18next";
-import { useSelector } from 'react-redux';
-import styles from "./Settings.style";
-import colors from "../../styles/colors";
-const langOptions = [
-  { label: "English", value: "en" },
-  { label: "Deutsch", value: "de" },
-  { label: "Spanish", value: "es" }
-]
-const themeOptions = [
-  { label: "Light", value: "light" },
-  { label: "Dark", value: "dark" },
-]
+import {View, Text, Appearance} from 'react-native';
+import SwitchSelector from 'react-native-switch-selector';
+import {useTranslation} from 'react-i18next';
+import {useDispatch, useSelector} from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import styles from './Settings.style';
+import colors from '../../styles/colors';
 
+const langOptions = [
+  {label: 'English', value: 'en'},
+  {label: 'Deutsch', value: 'de'},
+  {label: 'Spanish', value: 'es'},
+];
+const themeOptions = [
+  {label: 'Light', value: 'light'},
+  {label: 'Dark', value: 'dark'},
+  {label: 'Automatic', value: 'automatic'},
+];
+
+const storeData = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (e) {
+    // saving error
+  }
+};
 const Settings = () => {
   const theme = useSelector(state => state.theme);
+  const dispatch = useDispatch();
 
-  const { t, i18n } = useTranslation();
+  const {t, i18n} = useTranslation();
   return (
     <View style={styles[theme].container}>
       <View style={styles[theme].innerContainer}>
-        <Text style={styles[theme].title}>{t("Language")}</Text>
-        <SwitchSelector options={langOptions} initial={0} hasPadding bold buttonColor={colors[theme].accentColor} backgroundColor={colors[theme].secondaryBackgroundColor}
-          onPress={(languages) => {
+        <Text style={styles[theme].title}>{t('Language')}</Text>
+        <SwitchSelector
+          options={langOptions}
+          initial={0}
+          hasPadding
+          bold
+          buttonColor={colors[theme].accentColor}
+          backgroundColor={colors[theme].secondaryBackgroundColor}
+          onPress={languages => {
             i18n.changeLanguage(languages);
           }}
         />
       </View>
       <View style={styles[theme].innerContainer}>
-        <Text style={styles[theme].title}>{t("Theme")}</Text>
-        <SwitchSelector options={themeOptions} initial={0} hasPadding bold buttonColor={colors[theme].accentColor} backgroundColor={colors[theme].secondaryBackgroundColor} />
+        <Text style={styles[theme].title}>{t('Theme')}</Text>
+        <SwitchSelector
+          options={themeOptions}
+          initial={0}
+          hasPadding
+          bold
+          buttonColor={colors[theme].accentColor}
+          backgroundColor={colors[theme].secondaryBackgroundColor}
+          onPress={value => {
+            console.log(value);
+            storeData('theme', value);
+            if (value != 'automatic') {
+              dispatch({type: 'CHANGE_APP_THEME', payload: {theme: value}});
+              return;
+            }
+            dispatch({
+              type: 'CHANGE_APP_THEME',
+              payload: {theme: Appearance.getColorScheme()},
+            });
+          }}
+        />
       </View>
     </View>
   );
-}
-
+};
 
 export default Settings;
--- src/hooks/useAppStarted/useAppStarted.js
+++ src/hooks/useAppStarted/useAppStarted.js
@@ -1,17 +1,41 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {Appearance} from 'react-native';
 import {useDispatch} from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const useAppStarted = () => {
   const theme = Appearance.getColorScheme();
   const dispatch = useDispatch();
 
+  const getData = async key => {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        if (value != 'automatic') {
+          dispatch({type: 'CHANGE_APP_THEME', payload: {theme: value}});
+          return;
+        }
+        dispatch({
+          type: 'CHANGE_APP_THEME',
+          payload: {theme: Appearance.getColorScheme()},
+        });
+        return;
+      }
+      dispatch({
+        type: 'CHANGE_APP_THEME',
+        payload: {theme: Appearance.getColorScheme()},
+      });
+    } catch (e) {
+      // error reading value
+    }
+  };
+
   Appearance.addChangeListener(scheme => {
-    dispatch({type: 'CHANGE_APP_THEME', payload: {theme: scheme.colorScheme}});
+    getData('theme');
   });
   useEffect(() => {
-    dispatch({type: 'CHANGE_APP_THEME', payload: {theme: theme}});
-  },[])
+    getData('theme');
+  }, []);
 };
 
 export default useAppStarted;
