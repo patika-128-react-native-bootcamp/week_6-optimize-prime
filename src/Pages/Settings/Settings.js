f7c9a4112b4e31b88dd2c28d4698edd526cb00a3
omur aydin <ommuraydin@gmail.com>
2021-12-27 00:19:14 +0300

    language added to asyncStorage

--- src/Pages/Settings/Settings.js
+++ src/Pages/Settings/Settings.js
@@ -1,22 +1,11 @@
-import React from 'react';
-import {View, Text, Appearance} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Appearance } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
 import SwitchSelector from 'react-native-switch-selector';
-import {useTranslation} from 'react-i18next';
-import {useDispatch, useSelector} from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import styles from './Settings.style';
 import colors from '../../styles/colors';
-
-const langOptions = [
-  {label: 'English', value: 'en'},
-  {label: 'Deutsch', value: 'de'},
-  {label: 'Spanish', value: 'es'},
-];
-const themeOptions = [
-  {label: 'Light', value: 'light'},
-  {label: 'Dark', value: 'dark'},
-  {label: 'Automatic', value: 'automatic'},
-];
 
 const storeData = async (key, value) => {
   try {
@@ -25,17 +14,81 @@ const storeData = async (key, value) => {
     // saving error
   }
 };
-const Settings = () => {
-  const theme = useSelector(state => state.theme);
-  const dispatch = useDispatch();
 
-  const {t, i18n} = useTranslation();
+const Settings = () => {
+  const dispatch = useDispatch()
+  const { t, i18n } = useTranslation();
+  const [chosenLang, setChosenLang] = useState(0)
+  const [initialLang, setInitialLang] = useState()
+  const [chosenTheme, setChosenTheme] = useState(0)
+  const [initialTheme, setInitialTheme] = useState()
+  const theme = useSelector(state => state.theme);
+
+  const langOptions = [
+    { label: 'English', value: 'en' },
+    { label: 'Deutsch', value: 'de' },
+    { label: 'Spanish', value: 'es' },
+  ];
+  const themeOptions = [
+    { label: t('Light'), value: 'light' },
+    { label: t('Dark'), value: 'dark' },
+    { label: t('Automatic'), value: 'automatic' },
+  ];
+
+   useEffect(() => {
+    getData("languages", "theme")
+  }, [])
+
+  useEffect(() => {
+    if (initialLang === "en") {
+      setChosenLang(0)
+    } if (initialLang === "de") {
+      setChosenLang(1)
+    } if (initialLang === "es") {
+      setChosenLang(2)
+    }
+
+  }, [initialLang])
+
+  useEffect(() => {
+    if (initialTheme === "light") {
+      setChosenTheme(0)
+    } if (initialTheme === "dark") {
+      setChosenTheme(1)
+    } if (initialTheme === "automatic") {
+      setChosenTheme(2)
+    }
+
+  }, [initialTheme])
+
+  const getData = async (langKey, themeKey) => {
+    try {
+      const langValue = await AsyncStorage.getItem(langKey);
+      const themeValue = await AsyncStorage.getItem(themeKey);
+
+     
+      if (langValue !== null) {
+        setInitialLang(langValue)
+      }
+      if (themeValue !== null) {
+        setInitialTheme(themeValue)
+      }
+      console.log("themeValue", chosenTheme)
+
+
+
+    } catch (e) {
+      // error reading value
+    }
+  };
+
   return (
     <View style={styles[theme].container}>
       <View style={styles[theme].innerContainer}>
         <Text style={styles[theme].title}>{t('Language')}</Text>
         <SwitchSelector
           options={langOptions}
+          value={chosenLang}
           initial={0}
           hasPadding
           bold
@@ -43,6 +96,7 @@ const Settings = () => {
           backgroundColor={colors[theme].secondaryBackgroundColor}
           onPress={languages => {
             i18n.changeLanguage(languages);
+            storeData("languages", languages);
           }}
         />
       </View>
@@ -50,21 +104,21 @@ const Settings = () => {
         <Text style={styles[theme].title}>{t('Theme')}</Text>
         <SwitchSelector
           options={themeOptions}
+          value={chosenTheme}
           initial={0}
           hasPadding
           bold
           buttonColor={colors[theme].accentColor}
           backgroundColor={colors[theme].secondaryBackgroundColor}
           onPress={value => {
-            console.log(value);
             storeData('theme', value);
             if (value != 'automatic') {
-              dispatch({type: 'CHANGE_APP_THEME', payload: {theme: value}});
+              dispatch({ type: 'CHANGE_APP_THEME', payload: { theme: value } });
               return;
             }
             dispatch({
               type: 'CHANGE_APP_THEME',
-              payload: {theme: Appearance.getColorScheme()},
+              payload: { theme: Appearance.getColorScheme() },
             });
           }}
         />
--- src/hooks/useAppStarted/useAppStarted.js
+++ src/hooks/useAppStarted/useAppStarted.js
@@ -1,9 +1,11 @@
-import React, {useEffect} from 'react';
-import {Appearance} from 'react-native';
-import {useDispatch} from 'react-redux';
+import React, { useEffect } from 'react';
+import { Appearance } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const useAppStarted = () => {
+  const { t, i18n } = useTranslation();
   const theme = Appearance.getColorScheme();
   const dispatch = useDispatch();
 
@@ -12,19 +14,30 @@ const useAppStarted = () => {
       const value = await AsyncStorage.getItem(key);
       if (value !== null) {
         if (value != 'automatic') {
-          dispatch({type: 'CHANGE_APP_THEME', payload: {theme: value}});
+          dispatch({ type: 'CHANGE_APP_THEME', payload: { theme: value } });
           return;
         }
         dispatch({
           type: 'CHANGE_APP_THEME',
-          payload: {theme: Appearance.getColorScheme()},
+          payload: { theme: Appearance.getColorScheme() },
         });
         return;
       }
       dispatch({
         type: 'CHANGE_APP_THEME',
-        payload: {theme: Appearance.getColorScheme()},
+        payload: { theme: Appearance.getColorScheme() },
       });
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  const getLanguage = async key => {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        i18n.changeLanguage(value)
+      }
     } catch (e) {
       // error reading value
     }
@@ -35,6 +48,7 @@ const useAppStarted = () => {
   });
   useEffect(() => {
     getData('theme');
+    getLanguage('languages')
   }, []);
 };
 
