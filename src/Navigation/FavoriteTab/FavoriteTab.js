96069d055f47940ae43129388406d0a9f110a2dd
omur aydin <ommuraydin@gmail.com>
2021-12-27 00:20:18 +0300

    material top tab integrated

--- src/Navigation/FavoriteTab/FavoriteTab.js
+++ src/Navigation/FavoriteTab/FavoriteTab.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { useTranslation } from "react-i18next";
+import { useSelector } from 'react-redux';
+
+import fontSize from "../../styles/fontSize";
+import colors from "../../styles/colors";
+
 import FavoritesStack from '../FavoritesStack';
 import CharacterFavoritesStack from '../CharacterFavoritesStack';
 import routes from '../routes';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 const FavoriteTab = () => {
+  const { t, i18n } = useTranslation();
+  const theme = useSelector(state => state.theme);
+
   return (
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
-        tabBarStyle: {position: 'absolute', top:0},
-      }}>
-      <Tab.Screen name={routes.COMICS_FAVORITE} component={FavoritesStack} />
+        tabBarActiveTintColor: colors.accentColor,
+        tabBarInactiveTintColor: colors.textColor,
+        tabBarStyle: {
+          backgroundColor: colors.black,
+        },
+        tabBarLabelStyle: {
+          fontFamily: 'Proxima Nova Semibold',
+          fontSize: fontSize.small,
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: colors.accentColor,
+        }
+      }}
+    >
+      <Tab.Screen name={routes.COMICS_FAVORITE} component={FavoritesStack}
+        options={{
+          tabBarLabel: t("Favorite Comics"),
+        }}
+      />
       <Tab.Screen
         name={routes.CHARACTERS_FAVORITE}
         component={CharacterFavoritesStack}
+        options={{
+          tabBarLabel: t("Favorite Characters"),
+        }}
       />
     </Tab.Navigator>
   );
